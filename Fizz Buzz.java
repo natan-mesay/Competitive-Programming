5b28a3bcc0befe10f98111c25da0b61ad85867d1
natan-mesay <73078865+natan-mesay@users.noreply.github.com>
2022-12-26 18:04:13 +0300

    Create Fizz Buzz.java

---
+++ Fizz Buzz.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> match = new LinkedList<String>();
+        for (int i = 1; i <= n; i++) {
+            
+            if(i % 15 == 0){
+                match.add("FizzBuzz");
+            }
+            else if( i % 5 == 0){
+                match.add("Buzz");
+            }
+            else if(i % 3 == 0){
+                match.add("Fizz");
+            }
+            else{
+                match.add(""+i);
+            }
+}
+        return match;
+    }
+}

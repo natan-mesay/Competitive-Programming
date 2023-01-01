7cb951f2623dcd1fb7aff37ba27b9166c6cc1f17
Natan Mesay <natanmesay2000@gmail.com>
2023-01-01 20:10:52 +0300

    commit

---
+++ Week1/PowerofFour.java
@@ -0,0 +1,16 @@
+package Week1;
+
+
+public class PowerofFour {
+    public boolean isPowerOfFour(int n) {
+
+        while(n >= 1){
+            if(n == 1) return true;
+
+            else if(n%4 == 0) n/=4;
+
+            else break;
+        }
+        return false;
+    }
+}
